a79039b51b1d7e9139ca90cda43169700f88c49e
SurajNarang <60091462+SurajNarang@users.noreply.github.com>
2020-06-27 04:01:38 -0400

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -9,9 +9,12 @@ app.listen(3000, () => console.log("Listening at Port 3000"));
 // hosting files, serving from public directory
 app.use(express.static("public"));
 
+
+
 // call by hitting localhost:3000/lyft?startLat=47.6076018&startLong=-122.3119244&endLat=47.6233218&endLong=-122.3636521
 app.get("/lyft", (req, response) => {
   console.log(req.url);
+  res.sendFile(path.join(__dirname + '/index.html'));
   const {  startLat, startLong, endLat, endLong } = req.query;
   const url = `https://www.lyft.com/api/costs?start_lat=${startLat}&start_lng=${startLong}&end_lat=${endLat}&end_lng=${endLong}`;
 
@@ -43,5 +46,7 @@ app.get("/lyft", (req, response) => {
     }).catch(err => {
       console.log("Unable to retrieve price data");
     })
+    
 });
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')));
+
