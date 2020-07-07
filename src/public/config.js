1a0b606998cedf4b9dc075331b7e4acbc44bfb56
Snail2037 <sahilsinha2000@yahoo.com>
2020-07-07 19:43:02 -0400

    Updated luft

--- src/public/config.js
+++ src/public/config.js
@@ -1,6 +1,6 @@
 var config = {
     MY_KEY: 'AIzaSyBjKRJsSF2a1iw6ViOO-_NBQkfQwELr6vY',
-    LUFT_KEY: 'p2yhfbsb3u9kvh5kpz7z7b4x',
+    LUFT_KEY: '7tzr6qkhfrx7hhxfzyuzn4bz',
     SKYSCAN_KEY: 'f2a0fe483cmsh6dc8cbd8fda93d4p1035a1jsn72c0097275c9'
 
 }
