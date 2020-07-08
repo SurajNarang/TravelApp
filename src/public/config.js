05f5f84065cd5284da59674b5831a19432a5b8d8
Snail2037 <sahilsinha2000@yahoo.com>
2020-07-07 23:18:34 -0400

    Added bearer

--- src/public/config.js
+++ src/public/config.js
@@ -1,5 +1,5 @@
 var config = {
     MY_KEY: 'AIzaSyB7A5pyzuCpVYqEu8fMI32EJI3SwKZWeJU',
-    LUFT_KEY: '7tzr6qkhfrx7hhxfzyuzn4bz',
+    LUFT_KEY: 'Bearer xnws4wtnn7yjqym7m9fx5t88',
     SKYSCAN_KEY: 'f2a0fe483cmsh6dc8cbd8fda93d4p1035a1jsn72c0097275c9'
 }
