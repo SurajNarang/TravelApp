6ca433455dc2c135ad9faf66d08c5dc7fd1f2fd7
Snail2037 <sahilsinha2000@yahoo.com>
2020-07-07 20:04:06 -0400

    Updated

--- src/public/config.js
+++ src/public/config.js
@@ -1,6 +1,5 @@
 var config = {
-    MY_KEY: 'AIzaSyBjKRJsSF2a1iw6ViOO-_NBQkfQwELr6vY',
+    MY_KEY: 'AIzaSyB7A5pyzuCpVYqEu8fMI32EJI3SwKZWeJU',
     LUFT_KEY: '7tzr6qkhfrx7hhxfzyuzn4bz',
     SKYSCAN_KEY: 'f2a0fe483cmsh6dc8cbd8fda93d4p1035a1jsn72c0097275c9'
-
 }
