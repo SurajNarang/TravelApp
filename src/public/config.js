fac20eaa2ba538ddd53f92a82a51b66057f56890
Snail2037 <sahilsinha2000@yahoo.com>
2020-07-11 21:27:38 -0400

    Updated keys

--- src/public/config.js
+++ src/public/config.js
@@ -1,5 +1,5 @@
 var config = {
-    GOOGLE_KEY: 'AIzaSyAAtKqIl88E1kLsmKKH4uQCuM0kQYtR1hE',
+    GOOGLE_KEY: 'AIzaSyB7A5pyzuCpVYqEu8fMI32EJI3SwKZWeJUGULICK',
     LUFT_KEY: 'Bearer hd43pj4zk7muaes3rmnghutj',
     SKYSCAN_KEY: 'f2a0fe483cmsh6dc8cbd8fda93d4p1035a1jsn72c0097275c9'
 }
