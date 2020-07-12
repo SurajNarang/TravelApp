8b70038c4faee787c1af36954bfd5ba2390a83da
Daniel Kempner <dkempner@lyft.com>
2020-07-12 14:45:43 -0700

    remove config

--- src/public/config.js
+++
@@ -1,5 +0,0 @@
-var config = {
-    GOOGLE_KEY: 'AIzaSyB7A5pyzuCpVYqEu8fMI32EJI3SwKZWeJUGULICK',
-    LUFT_KEY: 'Bearer hd43pj4zk7muaes3rmnghutj',
-    SKYSCAN_KEY: 'f2a0fe483cmsh6dc8cbd8fda93d4p1035a1jsn72c0097275c9'
-}
