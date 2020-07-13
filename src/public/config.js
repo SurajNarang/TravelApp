0fbb9f83d3445589de8cf7b0ec5af689268e78d8
Snail2037 <sahilsinha2000@yahoo.com>
2020-07-13 14:24:53 -0400

    Updated keys

---
+++ src/public/config.js
@@ -0,0 +1,6 @@
+var config = {
+        GOOGLE_KEY: 'AIzaSyB7A5pyzuCpVYqEu8fMI32EJI3SwKZWeJU',
+        LUFT_KEY: 'Bearer q89eufbfzcqy5ggz2cmzhp9b',
+        SKYSCAN_KEY: 'f2a0fe483cmsh6dc8cbd8fda93d4p1035a1jsn72c0097275c9'
+    }
+    //src/public/config.js
